bfc4869b95a95f00867a7c8e14dc9a3b72f18b51
Ekant <ekantchandrakar07@gmail.com>
2020-12-31 13:29:45 +0530

    Level 3 security implement

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 console.log(process.env.API_KEY);
@@ -25,11 +25,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-//-------------mongoose-encryption---------------
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });//this will encrypt only Password
-//so here we re addding encryption package to userSchema
-//-------x------mongoose-encryption------x---------
 
 const User = new mongoose.model("User", userSchema);
 
@@ -50,7 +45,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if (err) {
@@ -63,7 +58,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
